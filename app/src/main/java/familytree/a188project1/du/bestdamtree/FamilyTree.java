6d70563086d756b639c3c99aef7468260d0f14b8
RemjeyLiu <jeremy.liu@drake.edu>
2018-11-15 10:16:18 -0600

    Created Login Screen for FamilyTree. Upon registering, the app will bring them straight into the app

---
+++ app/src/main/java/familytree/a188project1/du/bestdamtree/FamilyTree.java
@@ -0,0 +1,13 @@
+package familytree.a188project1.du.bestdamtree;
+
+import android.app.Application;
+
+import io.realm.Realm;
+
+public class FamilyTree extends Application {
+    public void onCreate() {
+        super.onCreate();
+
+        Realm.init(this);
+    }
+}
