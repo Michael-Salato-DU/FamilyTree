774154f9f4aedf61d779b7b8798c7fe9b4c56d19
Michael-Salato-DU <42783058+Michael-Salato-DU@users.noreply.github.com>
2018-11-20 10:16:53 -0600

    Added navigation menu
    Added a navigation menu to MainActivity. The menu options are: Profile, Your Trees, Helpful Hints, and Sign Out.

--- app/src/main/java/familytree/a188project1/du/bestdamtree/MainActivity.java
+++ app/src/main/java/familytree/a188project1/du/bestdamtree/MainActivity.java
@@ -1,13 +1,85 @@
 package familytree.a188project1.du.bestdamtree;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Declare variables
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // source for setting the toolbar as the action bar:
+        // title: Create a navigation drawer: Set the toolbar as the action bar
+        // author: Android Developers Documentation
+        // https://developer.android.com/training/implementing-navigation/nav-drawer#SetToolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        // end source
+
+        // source for adding the nav drawer button, a.k.a "hamburger menu"
+        // title: Create a navigation drawer: Add the nav drawer button
+        // author: Android Developers Documentation
+        // https://developer.android.com/training/implementing-navigation/nav-drawer#DrawerButton
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true); // enable app bar's "home" button
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp); // change button to use the "hamburger menu" icon
+        // end source
+
+        // source for handling clicks of navigation drawer items:
+        // title: Create a navigation drawer: Handle navigation click events
+        // author: Android Developers Documentation
+        // https://developer.android.com/training/implementing-navigation/nav-drawer#ListItemClicks
+
+        // Tie mDrawerLayout to DrawerLayout view
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Create a NavigationView and set its item selection listener
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setCheckable(true);
+                        //close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                }
+        );
+        // end source
+    }
+
+    // source for opening the navigation drawer when the "hamburger menu" is tapped:
+    // title: Create a navigation drawer: Open the drawer when the button is tapped
+    // author: Android Developers Documentation
+    // https://developer.android.com/training/implementing-navigation/nav-drawer#OpenDrawer
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
