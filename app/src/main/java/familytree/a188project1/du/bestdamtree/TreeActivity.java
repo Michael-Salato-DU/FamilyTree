d93091b758714694281a2e22e69dd264be865524
Michael-Salato-DU <42783058+Michael-Salato-DU@users.noreply.github.com>
2018-12-05 18:20:57 -0600

    TreeActivity has working nav menu
    Need to delete some old commented-out code still

--- app/src/main/java/familytree/a188project1/du/bestdamtree/TreeActivity.java
+++ app/src/main/java/familytree/a188project1/du/bestdamtree/TreeActivity.java
@@ -3,14 +3,21 @@ package familytree.a188project1.du.bestdamtree;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 
 import io.realm.Realm;
@@ -19,8 +26,12 @@ import io.realm.RealmResults;
 
 public class TreeActivity extends AppCompatActivity {
 
+    private RecyclerView treeList;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter treeAdapter;
+    private DrawerLayout mDrawerLayout;
     public Tree testFam;
-
+    public User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,9 +41,71 @@ public class TreeActivity extends AppCompatActivity {
         Realm realm = Realm.getDefaultInstance();
         String family = (String) getIntent().getStringExtra("family");
         testFam = realm.where(Tree.class).equalTo("name", family).findFirst();
+        // Get the user with the email address passed from MainActivity
+        String currentEmail = (String) getIntent().getStringExtra("current_email");
+        user = realm.where(User.class).equalTo("email", currentEmail).findFirst();
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // source for adding the nav drawer button, a.k.a "hamburger menu"
+        // title: Create a navigation drawer: Add the nav drawer button
+        // author: Android Developers Documentation
+        // https://developer.android.com/training/implementing-navigation/nav-drawer#DrawerButton
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true); // enable app bar's "home" button
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp); // change button to use the "hamburger menu" icon
+        // end source
+
+        // source for handling clicks of navigation drawer items:
+        // title: Create a navigation drawer: Handle navigation click events
+        // author: Android Developers Documentation
+        // https://developer.android.com/training/implementing-navigation/nav-drawer#ListItemClicks
+
+        // Tie mDrawerLayout to DrawerLayout view
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        // Create a NavigationView and set its item selection listener
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setCheckable(true);
+                        //close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+                        // end source
+
+                        // Open the appropriate activity based on the selected menu item
+                        // Open your profile to edit your card
+                        if (menuItem.getTitle().equals("Profile")) {
+                            Intent intent = new Intent(getBaseContext(), EditPersonActivity.class);
+                            intent.putExtra("person", user.getPerson().getRealmID());
+                            startActivity(intent);
+                        }
+                        // Open up your list of saved trees
+                        else if (menuItem.getTitle().equals("Your Trees")) {
+                            Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                            intent.putExtra("current_email", user.getEmail());
+                            startActivity(intent);
+                        }
+                        // Open up helpful hints
+                        else if (menuItem.getTitle().equals("Helpful Hints")) {
+                            Intent intent = new Intent(getBaseContext(), HelpfulHintsActivity.class);
+                            startActivity(intent);
+                        }
+                        // Sign out and return to the login page
+                        else if (menuItem.getTitle().equals("Sign Out")) {
+                            Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                            intent.putExtra("current_email", user.getEmail());
+                            startActivity(intent);
+                        }
+
+                        return true;
+                    }
+                }
+        );
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +117,13 @@ public class TreeActivity extends AppCompatActivity {
             }
         });
 
+        treeList = (RecyclerView) findViewById(R.id.tree_list);
+
+        layoutManager = new LinearLayoutManager(this);
+        treeList.setLayoutManager(layoutManager);
+        refreshList();
+
+        /*
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.addTab(tabLayout.newTab().setText(family));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
@@ -67,9 +147,45 @@ public class TreeActivity extends AppCompatActivity {
 
             }
         });
-
+        */
 
     }
 
+    // source for opening the navigation drawer when the "hamburger menu" is tapped:
+    // title: Create a navigation drawer: Open the drawer when the button is tapped
+    // author: Android Developers Documentation
+    // https://developer.android.com/training/implementing-navigation/nav-drawer#OpenDrawer
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void refreshList(){
+        Realm realm = Realm.getDefaultInstance();
+        final RealmList<Person> family = testFam.getPeople();
+        Log.d("refresh", "in RefreshList");
+        for (int i = 0; i<family.size(); i++){
+            Log.d("person", family.get(i).getFirstName());
+        }
+
+        RecyclerViewClickListener listener = new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Person person = (Person)family.get(position);
+                Intent intent = new Intent(view.getContext(), PersonActivity.class);
+                intent.putExtra("person",person.getRealmID());
+                intent.putExtra("family", testFam.getName());
+                startActivity(intent);
+            }
+        };
+
+        treeAdapter = new TreeListAdapter(this, family, listener);
+        treeList.setAdapter(treeAdapter);
+    }
 
 }
